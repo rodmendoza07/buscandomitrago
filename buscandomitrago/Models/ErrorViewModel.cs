95e82dea1fca4a42d1651b8796ad2c555208d108
rodmendoza07 <lr.mendozar@icloud.com>
2022-02-11 09:40:17 -0600

    creación de proyecto

---
+++ buscandomitrago/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace buscandomitrago.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
